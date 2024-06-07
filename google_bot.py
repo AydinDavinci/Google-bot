0f36d8111c3a3af0ca832de1e07f6cc457838133
Avr6g <143701285+AydinDavinci@users.noreply.github.com>
2024-06-07 10:23:30 +0200

    nu kan yana er mee kloten

--- google_bot.py
+++ google_bot.py
@@ -1,3 +1,20 @@
-import requests
 from bs4 import BeautifulSoup
-import sqlite3
+import requests
+
+url = "https://steam250.com/most_played"
+
+response = requests.get(url)
+html_content = response.content
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+game_titles = soup.find_all('span', class_='title')
+
+count = 1 
+
+for title in game_titles:
+    if count >= 251:
+        break
+    game_title_text = title.text.split('. ', 1)[-1] 
+    print(f'{game_title_text} staat op rank {count}')
+    count += 1
